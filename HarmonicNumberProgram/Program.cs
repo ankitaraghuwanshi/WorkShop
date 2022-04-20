85becd5829cd9ce59d2efbc8d54cc910c1d0380a
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-04-20 11:22:17 +0530

    [Ankita] Add: program for HarmonicNumber

---
+++ HarmonicNumberProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Harmonic
+{
+    class HarmonicNumber
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Enter the Number upto You want to print Harmonic number");
+            int number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                Console.Write(" 1/{0} +", i);
+            }
+           
+        }
+        
+    } 
+}
